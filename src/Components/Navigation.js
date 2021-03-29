b2cd8a20e7942a8c5387fe1dccc788168083503d
Kalvin <kalyon56300@gmail.com>
2021-03-29 10:38:42 +0200

    modif anim

--- src/Components/Navigation.js
+++ src/Components/Navigation.js
@@ -6,25 +6,34 @@ import {useState, useEffect} from 'react';
 import React from 'react';
 import { useHistory } from 'react-router-dom'
 import { useRef } from "react";
-
+import { gsap } from "gsap";
+import { CSSRulePlugin } from 'gsap/CSSRulePlugin';
+gsap.registerPlugin(CSSRulePlugin);
 
 function Navigation() {
 
   const menuRef = useRef(null);
   const history = useHistory();
+  // menuRef.current.setAttribute("disabled", "disabled");
+
 
   // Gérer le bon affichage des éléments lors du passage à une autre page
    useEffect(() => {
       return history.listen(() => {
-        let changeClass = menuRef.current;
-        setOpacity(0);
-        changeClass.classList.add('close');
+        // let changeClass = menuRef.current;
+        change();
+        // changeClass.classList.add('close');
       }) 
    }) 
 
   let page = useLocation().pathname;
   var pageBool = false;
   const [opacity, setOpacity] = useState(0);
+
+
+  const [disabled, setDisabled] = useState(false);
+  
+  
 
   if(page === "/"){
     pageBool = true;
@@ -48,23 +57,44 @@ function Navigation() {
 
 
   // Gérer l'opacité et les différentes classe du menu selon l'activation ou non du menu
+  // console.log(menuRef.current);
+  var tl = gsap.timeline();
+
+  // function timelineEnd(){
+  //   if(menuRef.current){
+  //     console.log('end');
+  //     console.log(menuRef.current);
+  //     // menuRef.current.setAttribute('disabled','none')
+  //   }
+  // }
+
+  
+  
   function change(){
-    // const style = getComputedStyle(refContainer.current)
-    // console.log(style.opacity);
-    let changeClass = menuRef.current;
-    // console.log('start : ' + changeClass.classList.value);
-    if(opacity === 0){
-      setOpacity(1);
-      if(changeClass.classList.value === "menuIconBlock"){
-        changeClass.classList.add('open');
+
+      // setDisabled(true);
+      
+
+      // if(menuRef.current){
+      //   menuRef.current.setAttribute('disabled','disabled');
+      // }
+      if(opacity === 0){
+        setOpacity(1);
+        tl.to(CSSRulePlugin.getRule('.menuIcon::before'), {duration: 0.4, cssRule: {translateY : '-1px'}},'start');
+        tl.to(CSSRulePlugin.getRule('.menuIcon::before'), {duration: 0.6, cssRule: {rotate : '45deg'}},'end');
+        tl.to(CSSRulePlugin.getRule('.menuIcon::after'), {duration: 0.4, cssRule: {translateY : '-1px'}},'start');
+        tl.to(CSSRulePlugin.getRule('.menuIcon::after'), {duration: 0.6, cssRule: {rotate : '-45deg'}},'end');
+        tl.to(CSSRulePlugin.getRule('.menuIcon'), {duration: 0.1, cssRule: {background : 'transparent'}}, 'start');
+        
       }else{
-        changeClass.classList.remove('close');
-      }      
-    }else{
-      setOpacity(0);
-      changeClass.classList.add('close');
-    }
-    // console.log('end : ' + changeClass.classList.value);
+        setOpacity(0);
+        tl.to(CSSRulePlugin.getRule('.menuIcon::before'), {duration: 0.4, cssRule: {translateY : '-10px', rotate : '0deg'}},'same');
+        tl.to(CSSRulePlugin.getRule('.menuIcon::after'), {duration: 0.4, cssRule: {translateY : '10px', rotate : '0deg'}},'same');
+        tl.to(CSSRulePlugin.getRule('.menuIcon'), {duration: 0.1, cssRule: {background : 'white'}}, 'same');
+      }
+      // setDisabled(false);
+
+    
     
   }
 
@@ -86,7 +116,7 @@ function Navigation() {
           <NavigationProfil/>
         </div>
         <div className="menu-small">
-          <div onClick={change} ref={menuRef} className="menuIconBlock">
+          <div ref={menuRef} onClick={change}  className="menuIconBlock">
             <div  className="menuIcon"></div>
           </div>
           <div /*ref={refContainer}*/ style={stl} className="menu-small-contain flex-center">
