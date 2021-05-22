e61058323be1e63c2ad226de99ab42819362ee13
Kalvin <kalyon56300@gmail.com>
2021-05-22 18:55:47 +0200

    ajout nouvelle disponibilite

--- src/Components/AppointmentDoctor.js
+++ src/Components/AppointmentDoctor.js
@@ -1,6 +1,49 @@
+import { withStyles } from '@material-ui/core/styles';
 import { IoCalendar } from "react-icons/io5";
+import Dialog from '@material-ui/core/Dialog';
+import MuiDialogTitle from '@material-ui/core/DialogTitle';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { MdClose } from "react-icons/md";
+import DialogContent from '@material-ui/core/DialogContent';
+import { TextField, OutlinedInput, InputLabel, FormControl } from '@material-ui/core';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import { MdVisibility, MdVisibilityOff  } from "react-icons/md";
+import { CircularProgress } from "@material-ui/core";
+import Alert from '@material-ui/lab/Alert';
+import { MuiPickersUtilsProvider, KeyboardDatePicker, TimePicker} from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import frLocale from "date-fns/locale/fr";
 
-function AppointmentDoctor(props) {
+function AppointmentDoctor({newAvailabilitie, handleClickOpenAvail, handleCloseAvail, openDialogAvail, availabilitie, handleAvailabilitieChange, erreur, success, loading}) {
+
+    const styles = (theme) => ({
+        root: {
+          margin: 0,
+          padding: theme.spacing(2),
+        },
+        closeButton: {
+          position: 'absolute',
+          right: theme.spacing(1),
+          top: theme.spacing(1),
+          color: theme.palette.grey[500],
+        },
+      });
+    
+    const DialogTitle = withStyles(styles)((props) => {
+        const { children, classes, onClose, ...other } = props;
+        return (
+            <MuiDialogTitle disableTypography className={classes.root} {...other}>
+            <Typography variant="h6">{children}</Typography>
+            {onClose ? (
+                <IconButton aria-label="close" className={classes.closeButton} onClick={onClose}>
+                <MdClose />
+                </IconButton>
+            ) : null}
+            </MuiDialogTitle>
+        );
+    });
+
     return (
         <div className="profil-appoint-block">
             <div className="profil-appoint-block-top flex">
@@ -12,7 +55,35 @@ function AppointmentDoctor(props) {
                 </div>
             </div>
             <div className="profil-appoint-block-content">
-
+                <div className="profil-appoint-availabilities flex-center">
+                    <button className="button-appoint-availabilities" onClick={handleClickOpenAvail}>+ Nouvelle disponibilité</button>
+                    <Dialog open={openDialogAvail} fullWidth>
+                        <DialogTitle onClose={handleCloseAvail}>Nouvelle disponibilité</DialogTitle>
+                        <DialogContent>
+                        <form className='form' onSubmit={newAvailabilitie}>
+                            <div className='div-form'>
+                                <MuiPickersUtilsProvider fullWidth utils={DateFnsUtils} locale={frLocale}>
+                                    <KeyboardDatePicker fullWidth format="dd/MM/yyyy" inputVariant="outlined" label="Date" value={availabilitie.start_date} onChange={(event) => handleAvailabilitieChange(event,"start_date")}></KeyboardDatePicker>
+                                </MuiPickersUtilsProvider>
+                            </div>
+                            <div className='div-form'>
+                                <MuiPickersUtilsProvider fullWidth utils={DateFnsUtils} locale={frLocale}>
+                                    <TimePicker label="Heure" inputVariant="outlined" fullWidth value={availabilitie.start_time} onChange={(event) => handleAvailabilitieChange(event,"start_time")} />
+                                </MuiPickersUtilsProvider>
+                            </div>
+                            <div className='div-form'>
+                                <TextField label="Durée (en minutes)" type="number" variant="outlined" fullWidth value={availabilitie.duration} onChange={(event) => handleAvailabilitieChange(event,"duration")} />
+                            </div>
+                            {erreur ? <div className='div-form'> <Alert severity="error">{erreur}</Alert> </div> : ""}
+                            {success ? <div className='div-form'> <Alert severity="success">Ajout réussi !</Alert> </div> : ""}
+                            <div className='div-form'>
+                                <button onClick={newAvailabilitie} className="btn-log flex-center"><span>Ajouter</span>{loading ? <CircularProgress size='22px' color="light" className='btn-load' /> : ""}</button>
+                            </div>
+                        </form>
+                        </DialogContent>
+                    </Dialog>
+                </div>
+                <div className="profil-appoint-list"></div>
             </div>
         </div>
     );
--- src/Components/DoctorProfil.js
+++ src/Components/DoctorProfil.js
@@ -1,15 +1,95 @@
+import { useState } from "react";
 import AppointmentDoctor from "./AppointmentDoctor";
 import InfosProfilDoctor from "./InfosProfilDoctor";
+import {http} from '../axios-create.js';
 
 
 function DoctorProfil({data}) {
+
+    const [openDialogAvail, setOpenDialogAvail] = useState(false);
+    const [availabilitie, setAvailabilitie] = useState({ 
+        start_date : new Date(),
+        start_time : new Date(),
+        duration : 60
+    });
+
+    const [loadingAvail, setLoadingAvail] = useState(false);
+    const [erreurAvail, setErreurAvail] = useState("");
+    const [successAvail, setSuccessAvail] = useState(false);
+
+    function newAvailabilitie(e){
+        e.preventDefault();//empeche le formulaire de rafraichir la page
+        setLoadingAvail(true);
+        const data = {
+            "appointment_date" : availabilitie.start_date,
+            "appointment_time" : availabilitie.start_time,
+            "appointment_duration" : availabilitie.duration
+        }
+        http.post('appointment/create', data)
+        .then((response) => {
+            setLoadingAvail(false);
+            console.log(response);
+            if(response.status === 20){
+                if(erreurAvail){
+                    setErreurAvail("");
+                }
+                setSuccessAvail(true);
+                handleCloseAvail();
+            }
+        }).catch((error) => {
+            setLoadingAvail(false);
+            if(successAvail){
+                setSuccessAvail(false);
+            }
+            if(error.response){
+                if(error.response.status !== 500){
+                    if(error.response.data.violations){
+                        setErreurAvail(error.response.data.violations[0].title);
+                    }
+                    if(error.response.data.message){
+                        setErreurAvail(error.response.data.message);
+                    }  
+                }else{
+                    setErreurAvail("Erreur interne au serveur");
+                }
+            }else{
+                setErreurAvail("Erreur interne au serveur");
+            }          
+        })
+    }
+
+    function handleClickOpenAvail(){
+        setOpenDialogAvail(true);
+    }
+    
+    function handleCloseAvail(){
+        setOpenDialogAvail(false);
+    }
+
+    function handleAvailabilitieChange(e,champ){
+        switch(champ){
+            case 'duration' :
+                setAvailabilitie({...availabilitie, duration: parseInt(e.target.value,10)});
+                break;            
+            case 'start_date':
+                setAvailabilitie({...availabilitie, start_date: new Date(e)});
+                break;
+            case 'start_time':
+                setAvailabilitie({...availabilitie, start_time: new Date(e)});
+                break;
+            default :
+                console.log("champ non valide");
+                break;
+        }
+    }
+
     return (
         <div className="block-profil flex-no-align-between">
             <div className="block-profil-left">
                 <InfosProfilDoctor data={data}/>
             </div>
             <div className="block-profil-right">
-                <AppointmentDoctor/>
+                <AppointmentDoctor newAvailabilitie={newAvailabilitie} handleClickOpenAvail={handleClickOpenAvail} handleCloseAvail={handleCloseAvail} openDialogAvail={openDialogAvail} availabilitie={availabilitie} handleAvailabilitieChange={handleAvailabilitieChange} loading={loadingAvail} success={successAvail} erreur={erreurAvail} />
             </div>
         </div>
     );
